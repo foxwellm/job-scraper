fa4f61b761c265e2e76bf3483cb337e05b857154
Matthew Foxwell <foxwellmw@gmail.com>
2019-06-03 12:16:42 -0600

    Update scrape all - completed

--- filter.js
+++ filter.js
@@ -5,8 +5,57 @@ let browser;
 let page;
 let content;
 const jobUrl = 'https://www.linkedin.com/jobs/search/?currentJobId=';
-const testJobs = ['1302424599', '1301816978', '1301816648', '1300230670', '1163296300', '1295012818', '1300230689', '1163803997', '1301822304', '1247367503'];
-// const testJobs = ['1302189677']
+// const testJobs = ['1302424599', '1301816978', '1301816648', '1300230670', '1163296300', '1295012818', '1300230689', '1163803997', '1301822304', '1247367503'];
+const testJobs = ['1279922107',
+  '1300680734',
+  '1289831292',
+  '1254170874',
+  '1284553529',
+  '1303737879',
+  '1283898959',
+  '1303341247',
+  '1262253398',
+  '1292860509',
+  '1240159129',
+  '1248887398',
+  '1297236071',
+  '1298133497',
+  '1267186746',
+  '1247367503',
+  '1298320177',
+  '1300230670',
+  '1258740029',
+  '1293901598',
+  '1284681053',
+  '1297276799',
+  '1304138756',
+  '1267119135',
+  '1256056569',
+  '1273035207',
+  '1253373749',
+  '1096427110',
+  '1283694938',
+  '1254997756',
+  '1262316990',
+  '1279464140',
+  '1173709315',
+  '1297216130',
+  '1254987955',
+  '1247902191',
+  '1300230689',
+  '1287296107',
+  '1163803997',
+  '1294584697',
+  '1229081648',
+  '869761839',
+  '1283153853',
+  '1293952685',
+  '1237080269',
+  '1295012818',
+  '1279398747',
+  '1290675075',
+  '1173751816',
+  '1298431908']
 // const testJobs = ['1301816648']
 // https://www.linkedin.com/jobs/search/?currentJobId=1302189677
 // https://www.linkedin.com/jobs/search/?currentJobId=1301816648
@@ -31,41 +80,48 @@ const startPuppeteer = async () => {
 
 const scrapePage = async () => {
   // console.log(await page.content())
-  let testBool = true;
-  try {
-    await page.waitForSelector('.topcard__title', { timeout: 10000 })
-    const pageContent = await page.evaluate(() => {
-      // const title = document.querySelector('.topcard__title').innerText
-      const title = 'title'
-      // const companyText = document.querySelector('.jobs-details-top-card__company-info a').innerText || document.querySelector('.jobs-details-top-card__company-info').innerText
-      const companyText = 'company'
-      const body = document.querySelector('.jobs-box__html-content span') ? document.querySelector('.jobs-box__html-content span').innerText : document.querySelector('.description__text').innerText
-      
-      // const body = 'body'
-      // return { title, companyText, body }
-      return false
-    })
-    // console.log(pageContent.title, pageContent.companyText, pageContent.body)
-    testBool = pageContent
-  } catch(error) {
-  
-    console.log(await page.content())
-    console.log(error)
-    console.log('Error scraping job')
-  }
-  return testBool
+  // let testBool = true;
+  // try {
+  //   await page.waitForSelector('.topcard__title', { timeout: 10000 })
+  const pageContent = await page.evaluate(() => {
+
+    const title = document.querySelector('.topcard__title') ? document.querySelector('.topcard__title').innerText : 'No title found'
+    // const title = 'title'
+    const companyText = document.querySelector('.jobs-details-top-card__company-info a') ? document.querySelector('.jobs-details-top-card__company-info a').innerText
+      : document.querySelector('.jobs-details-top-card__company-info') ? document.querySelector('.jobs-details-top-card__company-info').innerText
+        : document.querySelector('.topcard__flavor a') ? document.querySelector('.topcard__flavor a').innerText
+          : document.querySelector('.topcard__flavor') ? document.querySelector('.topcard__flavor').innerText
+            : 'No company name found'
+    // topcard__flavor
+    const body = document.querySelector('.jobs-box__html-content span') ? document.querySelector('.jobs-box__html-content span').innerText
+      : document.querySelector('.description__text') ? document.querySelector('.description__text').innerText
+        : 'No body found'
+
+    // const body = 'body'
+    return { title, companyText, body }
+    // return false
+  })
+  // console.log(pageContent.title, pageContent.companyText, pageContent.body)
+  return pageContent
+  // } catch (error) {
+
+  // console.log(await page.content())
+  //   console.log(error)
+  //   console.log('Error scraping job')
+  // }
+  // return pageContent
 }
 
 const iteratePages = async () => {
 
-  for(let job of testJobs) {
+  for (let job of testJobs) {
     console.log(`${jobUrl}${job}`)
     try {
       await page.goto(`${jobUrl}${job}`)
     } catch (error) {
       console.log(`Error loading page for ${job}`)
     }
-    await page.waitFor(3000);
+    await page.waitFor(4000);
     console.log('page loaded succesfully')
     let pageResults = await scrapePage()
     console.log(pageResults)
---
+++ reactJS.js
@@ -0,0 +1,125 @@
+const puppeteer = require('puppeteer');
+var fs = require("fs");
+
+let browser;
+let page;
+let allJobs = [];
+
+const startPuppeteer = async () => {
+  browser = await puppeteer.launch({ headless: false, defaultViewport: { width: 1680, height: 850, isLandscape: true, isMobile: false }, args: ['--disable-infobars, --disable-notifications'] });
+  page = await browser.newPage();
+  await page.emulateMedia('screen')
+  await page.setCacheEnabled(false)
+  page.setDefaultTimeout(6000)
+  page.setDefaultNavigationTimeout(6000)
+}
+
+const testElements = async () => {
+  console.log('test')
+  let returnedJobs
+  try {
+    await page.waitForSelector('[data-job-id]', { timeout: 2000 })
+    returnedJobs = await getStandardJobs()
+  } catch (error) {
+    try {
+      await page.waitForSelector('[data-id]', { timeout: 2000 })
+      returnedJobs = await getMobileJobs()
+    } catch (error) {
+      console.log('both failed')
+    }
+  }
+  return returnedJobs
+}
+
+const getStandardJobs = async () => {
+  const textContent = await page.evaluate(() => {
+    const all = document.querySelectorAll('[data-job-id]')
+    list = [];
+    all.forEach(item => {
+      list.push(item['dataset'].jobId)
+    })
+    return { list }
+  })
+  console.log('returning list of standard jobs')
+  return { list: textContent.list }
+}
+
+const getMobileJobs = async () => {
+  const textContent = await page.evaluate(() => {
+    const all = document.querySelectorAll('[data-id]')
+    list = [];
+    all.forEach(item => {
+      list.push(item['dataset'].id)
+    })
+    return { list }
+  })
+  console.log('returning list of mobile jobs')
+  return { list: textContent.list }
+}
+
+const runMethod = async () => {
+  await startPuppeteer()
+  await page.waitFor(2000);
+
+  let startNumber = 0;
+  let startPaginationNumber = 1
+  let passTest = false
+
+  await page.goto(`https://www.linkedin.com/jobs/react-js-jobs-united-states?keywords=react.js&f_TP=1&start=${startNumber}&count=25&trk=jobs_jserp_pagination_${startPaginationNumber}`)
+  await page.waitFor(3000);
+
+  try {
+    await page.waitForSelector('[data-job-id]', { timeout: 2000 })
+    console.log('standard passed')
+    passTest = true
+  } catch (error) {
+    console.log(error)
+  }
+
+  if (passTest) {
+
+    const endPaginationNumber = await page.evaluate(() => {
+      return document.querySelector('.pagination__pages').lastElementChild.innerText
+    })
+
+    let pageJobs = await testElements()
+    allJobs = [...allJobs, ...pageJobs.list]
+    startNumber = startNumber + 25
+    startPaginationNumber++
+
+    while (startPaginationNumber <= endPaginationNumber) {
+      await page.goto(`https://www.linkedin.com/jobs/react-js-jobs-united-states?keywords=react.js&f_TP=1&start=${startNumber}&count=25&trk=jobs_jserp_pagination_${startPaginationNumber}`)
+      await page.waitFor(3000);
+      console.log('page loaded')
+      let pageJobs = await testElements()
+      allJobs = [...allJobs, ...pageJobs.list]
+      startNumber = startNumber + 25
+      startPaginationNumber++
+    }
+
+
+    const currentTime = Date.now()
+
+    fs.writeFile(`./reactJSAll${currentTime}.js`, JSON.stringify(allJobs), function (err) {
+      if (err) {
+        console.log(err);
+      }
+      else {
+        console.log("Output saved to /reactJS.js.");
+      }
+    });
+  }
+
+
+
+  await console.log('allJobs', allJobs)
+  try {
+    await browser.close();
+
+  } catch (error) {
+    console.log('error closing')
+  }
+}
+
+runMethod()
+
